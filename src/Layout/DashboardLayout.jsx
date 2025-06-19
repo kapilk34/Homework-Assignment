02e0e4762f0627321d605578677ffc77c51fa352
kapilk34 <167847203+kapilk34@users.noreply.github.com>
2025-06-19 16:13:01 +0530

    Add files via upload

---
+++ src/Layout/DashboardLayout.jsx
@@ -0,0 +1,19 @@
+import { Link, Outlet } from "react-router-dom";
+
+const DashboardLayout=()=>{
+    return(
+        <div className="grid grid-cols-12 gap-2">
+            <div className="col-span-2 bg-red-500 text-white text-xl text-center">
+                <div className="bg-black py-5 font-bold text-xl">Sidebar</div>
+                <div className=" p-3 bg-red-300 rounded-md m-2 hover:bg-red-700 duration-100">
+                    <Link to="/dashboard">Dashboard</Link>
+                </div>        
+            </div>
+            <div className="col-span-10">
+                <Outlet/>
+            </div>
+        </div>
+    )
+}
+
+export default DashboardLayout
